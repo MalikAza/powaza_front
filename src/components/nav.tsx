6582101ea961afe052067d0d75b50630842446a4
Dakota <dakota742009@gmail.com>
2024-06-24 16:28:47 +0200

    feat: logo

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -33,7 +33,7 @@ export default function Nav() {
           <div className="flex-1 justify-between lg:flex-none mr-12">
             <a href="/" className="-m-1.5 p-1.5 flex items-center gap-x-1">
               <span className="sr-only">Powaza</span>
-              <span className='text-wet-asphalt dark:text-silver font-bold'>Powaza</span>
+              <img src="/images/powaza-logo.png" alt="Powaza Logo" height={40} width={40}/>
             </a>
           </div>
           <div className="flex lg:hidden">
@@ -91,9 +91,9 @@ export default function Nav() {
             >
               <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-silver dark:bg-dark-asphalt px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 transition-color duration-500 font-ubuntu">
                 <div className="flex items-center justify-between">
-                  <a href="#" className="-m-1.5 p-1.5 flex items-center gap-x-1">
+                  <a href="/" className="-m-1.5 p-1.5 flex items-center gap-x-1">
                     <span className="sr-only">Powaza</span>
-                    <span className='text-wet-asphalt dark:text-silver'>Powaza</span>
+                    <img src="/images/powaza-logo.png" alt="Powaza Logo" height={40} width={40}/>
                   </a>
                   <button
                     type="button"
